5db0ec6d0cac38e5824565a69d0323b367ca1ccb
Chih-Kai Liang <JohnnyLiang@Chih-KaideMacBook-Pro.local>
2015-05-13 18:38:26 -0600

    Add HelloWorld file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    
-    System.out.println("Hello GitHub World");
-    System.out.println("Testing...");
-    System.out.println("Testing for number 1 branch1");
-    System.out.println("Testing merge");  
+    System.out.println("Hello GitHub World"); 
   }
 }
